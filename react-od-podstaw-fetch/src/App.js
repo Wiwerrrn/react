217edbbecc95be5bf587ede9ccc5aefb2d0bbddb
eglapiak <eglapiak@man.poznan.pl>
2020-09-24 12:19:07 +0200

    lekcja

--- react-od-podstaw-fetch/src/App.js
+++ react-od-podstaw-fetch/src/App.js
@@ -3,37 +3,74 @@ import './App.css';
 
 
 // http://api.tvmaze.com/singlesearch/shows?q=girls
+function dataFetchReducer(state, action) {
+  switch(action.type) {
+    case "FETCH_START":
+      console.log('loading');
+      return {...state, loading: true}
+    case "FETCH_SUCCESS":
+      console.log(action.data)
+      return {
+        ...state,
+        data: action.data,
+        loading: false
+      }
+    case "FETCH_FAILED": 
+    console.log(action.error)
+      return {
+        ...state,
+        data: null,
+        loading: false,
+        error: action.error
+      }
+  }
+}
 
 function App() {
-  const [fetchedData, setFetchedData] = useState(null);
-  const [error, setError] = useState(null);
+  // const [fetchedData, setFetchedData] = useState(null);
+  // const [error, setError] = useState(null);
   const [movie, setMovie] = useState('girls');
-  const [isLoading, setIsLoading] = useState(true);
-
+  // const [isLoading, setIsLoading] = useState(true);
+  const [state, dispatch] = useReducer(
+    dataFetchReducer,
+    {
+      data: null,
+      error: null,
+      loading: true,
+    }
+  )
   useEffect(() => {
-  const myFetch = async () => {
-    setIsLoading(true);
+    const myFetch = async () => {
+    dispatch({type: "FETCH_START"});
+    //setIsLoading(true);
     
-    const response = await fetch(`http://api.tvmaze.com/singlesearch/shows?q=${movie}`);
+    
     try {
+      const response = await fetch(`http://api.tvmaze.com/singlesearch/shows?q=${movie}`);
       const responseParsed = await response.json();
-      setFetchedData(responseParsed);
-      setIsLoading(false);
+      //setFetchedData(responseParsed);
+      //setIsLoading(false);
+      dispatch({type: "FETCH_SUCCESS", data: responseParsed});
+
 
     } catch(error) {
-      setError(error);
-      setIsLoading(false);
-      console.log('lalla', error);
+      dispatch({type: "FETCH_FAILED", error: error});
+      //setError(error);
+      //setIsLoading(false);
+      //console.log('error:', error);
     }
   }
-    setIsLoading(false);
     myFetch();
   }, [movie]);
  
-  console.log(fetchedData);
+  //console.log('fetchedData',fetchedData);
   
-  if (isLoading) {
+  if (state.isLoading) {
     return <p> Loading...</p>
+  }
+
+  if (state.error) {
+    return <p> error</p>
   }
 
   return (
@@ -41,7 +78,7 @@ function App() {
     <h2>Movies</h2>
      <button 
       className="Button"
-      onClick={() => setMovie("*")}>
+      onClick={() => setMovie("game-of-thrones")}>
       change movie
      </button>
     </div>
